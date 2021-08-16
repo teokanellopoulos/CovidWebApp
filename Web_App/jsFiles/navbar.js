398def10bc863dbbab6fe89e2adfd08a26eb9237
Thodoris Kanellopoulos <56202077+teokanellopoulos@users.noreply.github.com>
2021-08-16 22:11:14 +0300

    Delete Web_App directory

--- Web_App/jsFiles/navbar.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementsByClassName("toggle")[0];
-const links = document.getElementsByClassName("links")[0];
-
-toggle.addEventListener("click", () => {
-    links.classList.toggle("active");
-});
